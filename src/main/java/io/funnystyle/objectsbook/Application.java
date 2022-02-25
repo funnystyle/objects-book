466a659db013f49f374118b9670b54e0aeadd132
Funnystyle <esero@hanmail.net>
2022-02-25 17:35:11 +0900

    rename

--- src/main/java/io/funnystyle/objectsbook/Application.java
+++ src/main/java/io/funnystyle/objectsbook/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ObjectsBookApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(ObjectsBookApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
