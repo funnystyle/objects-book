0036e77c06c831f2dcaa2ce87fcbf8e03a556a33
Funnystyle <esero@hanmail.net>
2022-02-25 17:34:38 +0900

    initial

---
+++ src/main/java/io/funnystyle/objectsbook/ObjectsBookApplication.java
@@ -0,0 +1,13 @@
+package io.funnystyle.objectsbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjectsBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ObjectsBookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/funnystyle/objectsbook/ObjectsBookApplicationTests.java
@@ -0,0 +1,13 @@
+package io.funnystyle.objectsbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObjectsBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
